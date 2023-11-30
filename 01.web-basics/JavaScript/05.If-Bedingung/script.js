5d663e4f3942bfc7d46b206e38dd5cbc99847e70
cengokong <scengizhan422@gmail.com>
2023-11-30 19:15:52 +0100

    05.If-Bedingung finished

---
+++ 01.web-basics/JavaScript/05.If-Bedingung/script.js
@@ -0,0 +1,9 @@
+let number = prompt("Geben Sie eine Zahl ein: ");
+
+if(number < 20){
+    console.log("mini");
+} else if(number < 50){
+console.log("Medium");
+}else{
+    console.log("Large");
+}
