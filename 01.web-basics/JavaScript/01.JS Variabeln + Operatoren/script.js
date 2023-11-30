8401ceed7f4e6b0b2907cf5f0537ce305a3155e9
cengokong <scengizhan422@gmail.com>
2023-11-30 08:24:20 +0100

    changes

---
+++ 01.web-basics/JavaScript/01.JS Variabeln + Operatoren/script.js
@@ -0,0 +1,50 @@
+// Hier nochmals die Aufgabe aus dem Video:
+// console.log("Hallo");
+// console.log("Tolle Sache");
+// // Variablen
+// let a = 7;
+// let b = 8;
+// let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+// (a + b) / c
+// c -a + b
+// a + b + c * 3
+// (a + b + c)/4
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+
+
+
+// Variabeln
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("A hat den Wert " +  a);
+
+a = 10;
+console.log("A hat den Wert " +  a);
+
+console.log("Hallo");
+console.log("Tolle Sache");
+
+console.log(a + b + c);
+console.log((a + b) / c);
+console.log(c - a + b);
+console.log(a + b + c * 3);
+console.log((a + b + c)/4);
+
+a = 3
+b = a + 3
+c = c -3
+
+console.log(a + b + c);
+console.log((a + b) / c);
+console.log(c - a + b);
+console.log(a + b + c * 3);
+console.log((a + b + c)/4);
