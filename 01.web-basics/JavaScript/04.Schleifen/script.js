b08945bc0b29e2ae3e629ea79ed4a08e338c0772
cengokong <scengizhan422@gmail.com>
2023-12-07 08:34:06 +0100

    04.Schleifen finished

--- 01.web-basics/JavaScript/04.Schleifen/script.js
+++ 01.web-basics/JavaScript/04.Schleifen/script.js
@@ -13,3 +13,16 @@ for(i =10; i >= 0; i--){
 for(i =1234; i >= 1207; i--){
     console.log(i);
 }
+
+for(let count = 0; count < 9; count++){
+    console.log(count + " Cengizhan");
+}
+
+for(i =0; i<= 10; i++){
+    console.log(i);
+}
+console.log("Cengizhan");
+
+for(i =10; i >= 0; i--){
+    console.log(i);
+}
