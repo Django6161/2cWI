c50c35c3657f5a5687cbe9981460cf4d40157e47
cengokong <scengizhan422@gmail.com>
2024-01-11 08:49:04 +0100

    08.Switch statement finished i think

--- 01.web-basics/JavaScript/08.Switch-Statement/script.js
+++ 01.web-basics/JavaScript/08.Switch-Statement/script.js
@@ -1,18 +1,15 @@
-let randomNumber = Math.random() *6;
-let random= Math.floor(randomNumber) + 5;
+let randomNumber = Math.random() * 4 + 7;
 
-if(randomNumber == 10){
-    console.log("Ten");
-} else if(randomNumber == 9){
-    console.log("Nine");
-} else if(randomNumber == 8){
-    console.log("Eight")
-}else if(randomNumber == 6){
+if(randomNumber <= 10 && randomNumber >= 9){
+    console.log("nine");
+} else if(randomNumber <= 9 && randomNumber >= 8){
+    console.log("eight");
+} else if(randomNumber <= 8 && randomNumber >= 7){
+    console.log("seven")
+}else if(randomNumber <= 7 && randomNumber >= 6){
     console.log("six");
-} else if(randomNumber == 7){
-    console.log("Seven");
-} else if(randomNumber == 5){
-    console.log("Five");
-}else{
-    console.log("Die Zahl liegt außerhalb des angegebenen Bereichs");
+} else if(randomNumber <= 6 && randomNumber >= 5){
+    console.log("five");
+} else if(randomNumber <= 11 && randomNumber >= 10){
+    console.log("ten");
 }
