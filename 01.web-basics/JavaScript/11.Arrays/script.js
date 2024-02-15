adf3f09d672cf8ccb53bde000ae0c7929fb63896
cengokong <scengizhan422@gmail.com>
2024-02-15 08:36:52 +0100

    11.arrays finished

---
+++ 01.web-basics/JavaScript/11.Arrays/script.js
@@ -0,0 +1,17 @@
+let a = 4;
+let b = 2;
+let c = 6;
+
+
+let arr = [4,9,8];
+console.log(arr[1]);
+
+
+arr.push(10);
+arr.push(3);
+
+
+
+for (let index = 0; index < arr.length; index++) {
+console.log(arr[index]);    
+}
