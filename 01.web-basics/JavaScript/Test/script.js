0a0e0ef5a8faa3e3fb30af69cbe232da3ae2d94f
cengokong <scengizhan422@gmail.com>
2024-04-25 09:00:33 +0200

    abgabe test

---
+++ 01.web-basics/JavaScript/Test/script.js
@@ -0,0 +1,15 @@
+const temparaturesWeek1 = -1 + 5 + 1 + -10 + -11 + 2 + 30 + -8 + -14;
+const temparaturesWeek2 = -1 + 5 + 3 + -10 + -11 + 2 + 30 + -8 + -44;
+
+console.log("Mittelwert 1. Woche: " + temparaturesWeek1 / 9);
+
+console.log("Mittelwert 2. Woche: " + temparaturesWeek2 / 9);
+
+console.log("Woche 1 war wärmer.");
+
+const data = "Hans geht heute schwimmen! Das ist gut.";
+console.log(data.slice(0, 4));
+console.log(data.slice(16, 25));
+console.log(data.slice(35, 38));
+
+const numbers = "8,2,1,4,6,11,8,7";
